89b5b1d46b5ded3984fa42351482212048f26cc2
西北一枝花 <1679659@qq.com>
2022-07-26 08:14:34 +0800

    修复`#抽卡版本`不发送渲染图片的问题

--- apps/version.js
+++ apps/version.js
@@ -1,10 +1,11 @@
-import { currentVersion, changelogs } from "../components/Changelog.js";
+import { currentVersion, changelogs, isV3} from "../components/Changelog.js";
 import Common from "../components/Common.js";
-
+import { segment } from "oicq";
 export async function versionInfo(e, { render }) {
-  return await Common.render("version/version-info", {
+  let base64 = await Common.render("version/version-info", {
     currentVersion,
     changelogs,
     elem: "cryo",
   }, { e, render, scale: 1.2 })
+  return isV3 ? e.reply(base64) : e.reply(segment.image(`base64://${base64}`));
 }
